f06373237d312d40927411188ee0088597b1e4e5
efert <efert269@gmail.com>
2020-04-28 17:19:34 +0700

    update fb.js

--- facebook.js
+++ facebook.js
@@ -49,7 +49,7 @@ function Facebook(accessToken, groupId)
             '\n' +
             '## thông tin\n' +
             '* Bài viết được tự động đăng lúc 12 giờ trưa hàng ngày\n' +
-            '* Đây là post tự động được tạo bởi efert và lấy dữ liệu từ VnExpress qua RSS các bạn có thể check source code [tại đây](https://bit.ly/lerni-bot-fb)');
+            '* Đây là post tự động được tạo bởi efert và lấy dữ liệu từ VnExpress qua RSS các bạn có thể check source code [tại đây](https://github.com/gadola/bot-up-fb.git)');
         formPublish.append('formatting', 'MARKDOWN');
 
         for (let i = 0; i < images.length ; i++) {
